18a314ae4f8df5165c1d7d94895903054657c123
Ewa Żukowska <ewa@MacBook-Pro-6.local>
2023-10-23 23:14:43 +0200

    12

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,10 +3,21 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args) {
         System.out.println("System wystartowal");
-        run();
+        run(args);
         System.out.println("System zakonczyl dzialanie");
     }
-    public static void run() {
+    public static void run(String[] args) {
         System.out.println("Zwierzak idzie do przodu");
+
+        if (args.length > 0) {
+            StringBuilder argumentString = new StringBuilder();
+            argumentString.append(args[0]);
+            for (int i = 1; i < args.length; i++) {
+                argumentString.append(", ").append(args[i]);
+            }
+            System.out.println("Argumenty: " + argumentString);
+        } else {
+            System.out.println("Brak przekazanych argumentów.");
+        }
     }
 }
