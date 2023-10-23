495d20f036bd5519924f7386da2f366ebe4984cf
Ewa Żukowska <ewa@MacBook-Pro-6.local>
2023-10-23 22:50:02 +0200

    7

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,9 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("System wystartowal");
+        System.out.println("System zakonczyl dzialanie");
+    }
+
+    }
