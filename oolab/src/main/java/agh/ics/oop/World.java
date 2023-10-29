8308582c622f2b8b14bc052b7f316e71bbe2d144
ewazukowska <ewa795738@wp.pl>
2023-10-29 12:37:55 +0100

    jeszcze wersja bez testów

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,13 +1,15 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
+import agh.ics.oop.model.MapDirection;
+
+import java.util.Arrays;
 
 public class World {
     public static void main(String[] args) {
         System.out.println("System wystartował");
         MoveDirection[] directions = OptionsParser.parse(args);
-
-        System.out.println("Zwierzak idzie do przodu");
 
         for (MoveDirection direction : directions) {
             if (direction != null) {
@@ -21,6 +23,18 @@ public class World {
         }
 
         System.out.println("System zakończył działanie");
+
+        Vector2d position1 = new Vector2d(1,2);
+        System.out.println(position1);
+        Vector2d position2 = new Vector2d(-2,1);
+        System.out.println(position2);
+        System.out.println(position1.add(position2));
+
+        System.out.println("Początkowy kierunek: " + MapDirection.NORTH);
+        System.out.println("Następny kierunek: " + MapDirection.NORTH.next());
+        System.out.println("Poprzedni kierunek: " + MapDirection.NORTH.previous());
+        System.out.println("Wektor jednostkowy " + MapDirection.NORTH.toUnitVector());
+
     }
 }
 
