ec17bf0d85fb639488422b848cb6a8734cbb3bba
ewazukowska <ewa795738@wp.pl>
2023-11-07 17:17:12 +0100

    bardzo przepraszam za 17* min spoznienia

--- oolab/src/main/java/agh/ics/oop/OptionsParser.java
+++ oolab/src/main/java/agh/ics/oop/OptionsParser.java
@@ -1,4 +1,6 @@
-package agh.ics.oop.model;
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
 
 public class OptionsParser {
     public static MoveDirection[] parse(String[] args) {
--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,7 +1,6 @@
 package agh.ics.oop;
 
 import agh.ics.oop.model.MoveDirection;
-import agh.ics.oop.model.OptionsParser;
 import agh.ics.oop.model.Vector2d;
 import agh.ics.oop.model.MapDirection;
 
